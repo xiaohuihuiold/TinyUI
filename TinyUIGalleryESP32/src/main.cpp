a81508278ca9ef1213daf92bb0955a96f90518c3
xiaohuihuiold <xiaohuihuiold@gmail.com>
2023-07-23 00:57:26 +0800

    feat(TinyUIGalleryESP32): 修改u8g2为gfx

--- TinyUIGalleryESP32/src/main.cpp
+++ TinyUIGalleryESP32/src/main.cpp
@@ -1,14 +1,34 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 #include <TinyUI.h>
-#include <U8g2lib.h>
+#include <Wire.h>
 
 #include "ESP32TimerBinding.h"
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
 using namespace tiny;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   Serial.begin(115200);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+
   TinyUI::begin(new ESP32TimerBinding());
 }
 
-void loop() {}
+void loop() {
+  display.clearDisplay();
+  display.drawPixel(10, 10, SSD1306_WHITE);
+  display.display();
+}
